b6124e3cec71edfe71095f1b13042ef3b21f10cf
CristianEspejel <19013376@es.uveg.edu.mx>
2023-04-25 10:35:33 -0600

    Initial Commit

---
+++ Que personaje eres?/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Que personaje eres?
+//
+//  Created by Cristian Michael Espejel Iglesias on 25/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Que personaje eres?UITests/Que_personaje_eres_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Que_personaje_eres_UITestsLaunchTests.swift
+//  Que personaje eres?UITests
+//
+//  Created by Cristian Michael Espejel Iglesias on 25/04/23.
+//
+
+import XCTest
+
+final class Que_personaje_eres_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
